914c95d79453f7c428301357979863236c16880c
Youe Graillot <youe.graillot@gmail.com>
2020-11-28 13:07:19 +0100

    [Audio] Merge reactors & Adaptive volume

---
+++ Assets/Scripts/Audio/AudioSourceController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSourceController : MonoBehaviour
+{
+    [SerializeField]
+    float m_transitionTime;
+    [SerializeField]
+    AnimationCurve m_volumeCurve;
+
+    AudioSource m_source;
+
+    private bool m_volumeActive = false;
+    private float m_currentTime = 0;
+
+    private void Awake()
+    {
+        m_source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        m_currentTime = Mathf.Clamp01(m_currentTime + ((m_volumeActive ? Time.deltaTime : -Time.deltaTime) / m_transitionTime));
+        m_source.volume = m_volumeCurve.Evaluate(m_currentTime);
+    }
+
+    public void SetVolume(bool enabled)
+    {
+        m_volumeActive = enabled;
+    }
+}
